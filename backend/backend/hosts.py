7e702c2674d5294f02dcc43af2de1e9b54430a17
ninjavroom <ninjavroom@gmail.com>
2022-10-02 23:49:54 +0000

    with hosts.py cms

--- backend/backend/hosts.py
+++ backend/backend/hosts.py
@@ -5,7 +5,7 @@ host_patterns = patterns('',
     host(r'www', settings.ROOT_URLCONF, name='www'),
     # host(r'(\w+)', 'path.to.custom_urls', name='wildcard'),
     host(r'api', 'api.urls', name='api'),
-    host(r'systems-backend-admin', 'django.contrib.admin.site.urls', name='ADMIN'),
+    host(r'cms', 'django.contrib.admin.site.urls', name='ADMIN'),
 )
 # akwaaba
 # akwaabaapi
